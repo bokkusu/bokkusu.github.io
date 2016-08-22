b23dfa69e50e7ac09269bca25231b81baa61332f
bokkusu <albert.chow@gmail.com>
2016-08-22 11:51:43 -0400

    Update bloks.js

--- arcade/boksbloks/js/bloks.js
+++ arcade/boksbloks/js/bloks.js
@@ -1,1 +1,9 @@
+// https://developer.mozilla.org/en-US/docs/Games/Anatomy
 
+window.main = function () {
+  window.requestAnimationFrame( main );
+  
+  // Whatever your main loop needs to do.
+};
+
+main(); //Start the cycle.

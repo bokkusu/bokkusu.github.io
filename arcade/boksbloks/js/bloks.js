287f9cd4e7a8774247a45e5a03ba3f75dd4211fc
bokkusu <albert.chow@gmail.com>
2016-08-30 16:13:54 -0400

    Update bloks.js

--- arcade/boksbloks/js/bloks.js
+++ arcade/boksbloks/js/bloks.js
@@ -4,6 +4,61 @@ window.main = function () {
   window.requestAnimationFrame( main );
   
   // Whatever your main loop needs to do.
+  update();
+  drawBoard();
 };
 
 main(); //Start the cycle.
+
+piece = null;
+pieceXY = [0,0];
+
+function update() {
+  if (piece == null) {
+    piece = getPiece();
+    pieceXY = [3,0];
+  }
+  else {
+    var x = pieceXY[0];
+    var y = pieceXY[1];
+
+    for (var j = 0; j < piece.length; ++j) {
+      for (var i = 0; i < piece[j].length; ++i) {
+        board[x+i][y+j] = 0;
+      }
+    }
+
+    y = y + 1;
+    pieceXY = [x,y];
+
+    for (var j = 0; j < piece.length; ++j) {
+      for (var i = 0; i < piece[j].length; ++i) {
+        board[x+i][y+j] = 1;
+      }
+    }
+
+  }
+}
+
+function getPiece() {
+  var min = Math.ceil(0);
+  var max = Math.floor(7);
+  var piece = Math.floor(Math.random() * (max - min)) + min;
+  switch (piece) {
+    case 0: return zpiece();
+    case 1: return spiece();
+    case 2: return lpiece();
+    case 3: return jpiece();
+    case 4: return opiece();
+    case 5: return ipiece();
+    case 6: return tpiece();
+  }
+}
+
+function zpiece { return  [[1,1,0],[0,1,1]];}
+function spiece { return  [[0,1,1],[1,1,0]];}
+function lpiece { return  [[0,0,1],[1,1,1]];}
+function jpiece { return  [[1,0,0],[1,1,1]];}
+function opiece { return  [[1,1],[1,1]];}
+function ipiece { return  [[1,1,1,1]];}
+function tpiece { return  [[0,1,0],[1,1,1]];}
